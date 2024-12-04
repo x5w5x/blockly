00c4412da22adeab49007d7d2875920ee1125bdf
慕芸婷 <12344332+xx3457388139@user.noreply.gitee.com>
2024-12-04 14:39:42 +0800

    调整

--- src/test/js/blockly.js
+++ src/test/js/blockly.js
@@ -1,63 +1,88 @@
-// blockly相关设置
 
+  //工具箱设置
 
-const toolbox = {
-    "kind": "flyoutToolbox",
-    "contents": [
-      {
-        "kind": "block",
-        "type": "controls_if"
-      },
-      {
-        "kind": "block",
-        "type": "controls_repeat_ext"
-      },
-      {
-        "kind": "block",
-        "type": "logic_compare"
-      },
-      {
-        "kind": "block",
-        "type": "math_number"
-      },
-      {
-        "kind": "block",
-        "type": "math_arithmetic"
-      },
-      {
-        "kind": "block",
-        "type": "text",
-        
-      },
-      {
-        "kind": "block",
-        "type": "text_print",
-        
-      },
-      {
-        "kind": "block",
-        "type": "logic_boolean"
-      },
-  
-  
-  {"kind": "block",
-  "type": "controls_for",
-        "fields": {
-          "VAR": {
-            "name": "index",
-            "type": "Number"
-          }
+var toolbox = {
+  "kind": "categoryToolbox",
+  "contents": [
+    {
+      "kind": "category",
+      "name": "基础",
+      "colour":230,
+      "contents": [
+        {
+          "kind": "block",
+          "type": "controls_if"
+        },
+      ]
+    },
+    {
+      "kind": "category",
+      "name": "计算",
+      "colour":250,
+      "contents": [
+        {
+          "kind": "block",
+          "type": "logic_compare"
+        },
+        {
+          "kind": "block",
+          "type": "logic_operation"
+        },
+        {
+          "kind": "block",
+          "type": "logic_boolean"
         }
-      },
-      {
-        "kind": "button",
-        "text": "A button",
-        "callbackKey": "myFirstButtonPressed"
-      },
-  
-  
-    ]}
-  
+      ]
+    }
+  ]
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  //设置共作区
   var workspace = Blockly.inject('blocklyDiv',{
       toolbox:toolbox,
       media:'https://unpkg.com/blockly/media/',
@@ -92,21 +117,31 @@ const toolbox = {
       
   })
   
+  // 动态调整大小
   
   
-  // 
-//   
-// 
-workspace.addChangeListener(function (event) {
-    // 检查事件类型
-    if (event.type == Blockly.Events.CHANGE ||
-        event.type == Blockly.Events.CREATE ||
-        event.type == Blockly.Events.DELETE) {
-      // 更新代码
-      // updateCode();
-    }
-  });
+  const onresize = function(e) {
+    let element = blocklyArea;
+    let x = 0;
+    let y = 0;
+    do {
+      x += element.offsetLeft;
+      y += element.offsetTop;
+      element = element.offsetParent; 
+    } while (element);
+    blocklyDiv.style.left = x + 'px';
+    blocklyDiv.style.top = y + 'px';
+    blocklyDiv.style.width = blocklyArea.offsetWidth + 'px';
+    blocklyDiv.style.height = blocklyArea.offsetHeight + 'px';
+    Blockly.svgResize(workspace);
+  };
+  window.addEventListener('resize', onresize, false);
+  onresize();
+  
 
+ 
+
+//代码显示
 
 function myUpdateFunction(event) {
   var code = Blockly.Python.workspaceToCode(workspace);
